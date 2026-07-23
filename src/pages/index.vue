b155779301a54dde4a2c9abb0049ca27b0a6363c
zhazong <8159863+zhazong@user.noreply.gitee.com>
2026-07-23 18:58:49 +0800

    draw-line

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -13,99 +13,20 @@
         />
 
         <q-toolbar-title v-if="$q.screen.gt.xs" shrink class="row items-center no-wrap">
-          <!-- <img src="https://cdn.quasar.dev/img/layout-gallery/logo-google.svg" /> -->
-          <span class="q-ml-sm">TP-Map</span>
+          <img src="logo/logo-google.svg" />
+          <span class="q-ml-sm">TPMap</span>
         </q-toolbar-title>
 
         <q-space />
 
         <q-space />
-
-        <div class="q-gutter-sm row items-center no-wrap">
-          <q-btn v-if="$q.screen.gt.sm" round dense flat icon="apps">
-            <q-tooltip>Google Apps</q-tooltip>
-          </q-btn>
-          <q-btn round dense flat color="grey-8" icon="notifications">
-            <q-badge color="red" text-color="white" floating> 2 </q-badge>
-            <q-tooltip>Notifications</q-tooltip>
-          </q-btn>
-          <q-btn round flat>
-            <q-avatar size="26px">
-              <img src="static/imgs/avatar/rikka.gif" />
-            </q-avatar>
-            <q-tooltip>Account</q-tooltip>
-          </q-btn>
-        </div>
+        <HeaderRightContent />
       </q-toolbar>
     </q-header>
 
     <q-drawer v-model="leftDrawerOpen" show-if-above bordered class="bg-dark" :width="280">
       <q-scroll-area class="fit">
-        <q-list padding dense>
-          <q-item
-            class="GNL__drawer-item"
-            v-ripple
-            v-for="link in links1"
-            :key="link.text"
-            clickable
-          >
-            <q-item-section avatar>
-              <q-icon :name="link.icon" />
-            </q-item-section>
-            <q-item-section>
-              <q-item-label>{{ link.text }}</q-item-label>
-            </q-item-section>
-          </q-item>
-
-          <q-separator inset class="q-my-sm" />
-
-          <q-item
-            class="GNL__drawer-item"
-            v-ripple
-            v-for="link in links2"
-            :key="link.text"
-            clickable
-          >
-            <q-item-section avatar>
-              <q-icon :name="link.icon" />
-            </q-item-section>
-            <q-item-section>
-              <q-item-label>{{ link.text }}</q-item-label>
-            </q-item-section>
-          </q-item>
-
-          <q-separator inset class="q-my-sm" />
-
-          <q-item
-            class="GNL__drawer-item"
-            v-ripple
-            v-for="link in links3"
-            :key="link.text"
-            clickable
-          >
-            <q-item-section>
-              <q-item-label
-                >{{ link.text }} <q-icon v-if="link.icon" :name="link.icon"
-              /></q-item-label>
-            </q-item-section>
-          </q-item>
-
-          <div class="q-mt-md">
-            <div class="flex flex-center q-gutter-xs">
-              <a class="GNL__drawer-footer-link" href="javascript:void(0)" aria-label="Privacy"
-                >Privacy</a
-              >
-              <span> · </span>
-              <a class="GNL__drawer-footer-link" href="javascript:void(0)" aria-label="Terms"
-                >Terms</a
-              >
-              <span> · </span>
-              <a class="GNL__drawer-footer-link" href="javascript:void(0)" aria-label="About"
-                >About Google</a
-              >
-            </div>
-          </div>
-        </q-list>
+        <LeftDrawerContent />
       </q-scroll-area>
     </q-drawer>
 
@@ -121,24 +42,13 @@
 
 <script setup>
 import { ref } from 'vue'
-import { fasEarthAmericas } from '@quasar/extras/fontawesome-v7'
-import MapView from '@/components/map-view.vue'
-import ToolbarRight from '@/components/toolbar-right.vue'
-import ToolbarLeftTop from '@/components/toolbar-left-top.vue'
+import MapView from '@/components/view/map-view.vue'
+import ToolbarRight from '@/components/bar/toolbar-right.vue'
+import ToolbarLeftTop from '@/components/bar/toolbar-left-top.vue'
+import LeftDrawerContent from '@/components/content/left-drawer-content.vue'
+import HeaderRightContent from '@/components/content/header-right-content.vue'
 
 const leftDrawerOpen = ref(false)
-
-const links1 = [{ icon: 'web', text: 'Top stories' }]
-const links2 = [
-  { icon: 'flag', text: 'Canada' },
-  { icon: fasEarthAmericas, text: 'World' },
-  { icon: 'place', text: 'Local' },
-]
-const links3 = [
-  { icon: '', text: 'Language & region' },
-  { icon: '', text: 'Settings' },
-]
-
 function toggleLeftDrawer() {
   leftDrawerOpen.value = !leftDrawerOpen.value
 }
@@ -148,7 +58,7 @@ function toggleLeftDrawer() {
 .GNL
 
   &__toolbar
-    height: 64px
+    height: 48px
 
   &__toolbar-input
     width: 55%
